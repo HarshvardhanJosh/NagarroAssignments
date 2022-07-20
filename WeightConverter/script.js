1398f2915c0d632682cd6d8cbe5ab773cd0b383a
HarshvardhanJosh <harshitjoshi001@gmail.com>
2022-07-20 15:30:29 +0530

    weight converter

---
+++ WeightConverter/script.js
@@ -0,0 +1,6 @@
+document.getElementById("output").style.visibility = "hidden";
+document.getElementById("lbsInput").addEventListener("input", function(e) {
+  document.getElementById("output").style.visibility = "visible";
+  let lbs = e.target.value;
+  document.getElementById("kgOutput").innerHTML = lbs / 1000;
+});
